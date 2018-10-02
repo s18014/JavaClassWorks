0eb6042d2b841aaae98660312cbdd61177caad73
s18014 <s18014@std.it-college.ac.jp>
2018-10-02 07:53:58 +0000

    added kadai in 2018-10-02

---
+++ 1002/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        String space = "";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(space + "Hello World!");
+            space += " ";
+        }
+    }
+}
