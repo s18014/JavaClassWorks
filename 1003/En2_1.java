449ea108b16c51c37e12ab4cfa02c1059cd07cac
Ryu <s17001@std.it-college.ac.jp>
2018-10-10 11:08:20 +0900

    Update En2_1.java

--- 1003/En2_1.java
+++ 1003/En2_1.java
@@ -4,7 +4,7 @@ public class En2_1{
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
-                out.println("FuzzBuzz");
+                out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 out.println("Fizz");
             } else if (i % 5 == 0) {
